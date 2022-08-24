e2c03c2387f0c53a09a67729402daafa16af7d4e
satya679 <narayanareddy27@gmil.com>
2022-08-24 13:17:00 +0530

    cosdid

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "cliet-cotext" {
+  source = "github.com/satya679/cotext"
+
+  ladscape = var.ladscape
+  environment = var.environment
+  region = var.region
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ladscape" {
+  default = null
+  description = "returs the data relavat to the target ladscape "
+  type = string
+}
+variable "environment" {
+  default = null
+  description = "returs the data relavat to the target eviromet"
+  type = string
+}
+variable "region" {
+  default = null
+  description = "returs the data relavat to the target regio"
+  type = string
+}
